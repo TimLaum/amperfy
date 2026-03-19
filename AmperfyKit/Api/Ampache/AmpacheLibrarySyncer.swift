c3c98f82b0e0efe5d92e22d8e93d209461561282
Edouard Lafargue <edouard@lafargue.name>
2026-03-19 00:20:58 -0700

    API: support GetSimilarSongs2 (aka Instant Mix) (#641)

--- AmperfyKit/Api/Ampache/AmpacheLibrarySyncer.swift
+++ AmperfyKit/Api/Ampache/AmpacheLibrarySyncer.swift
@@ -1008,6 +1008,39 @@ class AmpacheLibrarySyncer: CommonLibrarySyncer, LibrarySyncer {
   }
 
   @MainActor
+  func requestSimilarSongs(song: Song, count: Int) async throws -> [Song] {
+    guard isSyncAllowed else { return [] }
+    let response = try await ampacheXmlServerApi.requestSimilarSongs(id: song.id, count: count)
+
+    let similarSongObjectIds: [NSManagedObjectID] = try await storage.async.performAndGet {
+      asyncCompanion in
+      let accountAsync = asyncCompanion.library.getAccount(managedObjectId: self.accountObjectId)
+      let idParserDelegate = IDsParserDelegate(performanceMonitor: self.performanceMonitor)
+      try self.parse(
+        response: response,
+        delegate: idParserDelegate,
+        isThrowingErrorsAllowed: false
+      )
+      let prefetch = asyncCompanion.library.getElements(
+        account: accountAsync,
+        prefetchIDs: idParserDelegate.prefetchIDs
+      )
+
+      let parserDelegate = SongParserDelegate(
+        performanceMonitor: self.performanceMonitor, prefetch: prefetch, account: accountAsync,
+        library: asyncCompanion.library,
+        parseNotifier: nil
+      )
+      try self.parse(response: response, delegate: parserDelegate)
+      return parserDelegate.parsedSongs.compactMap { $0.managedObject.objectID }
+    }
+
+    return try similarSongObjectIds.map {
+      Song(managedObject: try storage.main.context.existingObject(with: $0) as! SongMO)
+    }
+  }
+
+  @MainActor
   func requestPodcastEpisodeDelete(podcastEpisode: PodcastEpisode) async throws {
     guard isSyncAllowed else { return }
     let response = try await ampacheXmlServerApi.requestPodcastEpisodeDelete(id: podcastEpisode.id)
